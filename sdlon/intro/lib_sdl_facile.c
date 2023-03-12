1bf54e8803634dca5f01a178e783112d29ef81e0
LilianGit <liliancolla@gmail.com>
2023-03-12 11:26:18 +0100

    dossier intro

--- sdlon/intro/lib_sdl_facile.c
+++ sdlon/intro/lib_sdl_facile.c
@@ -2,7 +2,7 @@
 # include <SDL2/SDL.h>
 # include <SDL2/SDL_ttf.h>
 # include <SDL2/SDL_image.h>
-# include "./lib/lib_sdl_facile.h"
+# include "../lib/lib_sdl_facile.h"
 
 /**
   \file lib_sdl_facile.c
