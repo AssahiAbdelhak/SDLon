b454db3307f36c4dfbf3ccb3786d914c1a3428de
NAME HERE <wballereau@gmail.com>
2023-03-14 15:13:33 +0100

    suppression des sdlons d'un set

--- sdlon/include/player.h
+++ sdlon/include/player.h
@@ -26,12 +26,13 @@ player_t player_create(char * name, int genre, char * file_name);
 player_t player_init(char * file_name);
 int player_quit(player_t * p);
 int add_sdlon_in_set(sdlon sd, player_t * player);
-int remove_sdlon_in_set(sdlon sd);
-int switch_sdlon_from_set(sdlon sd);
+int remove_sdlon_in_set(sdlon sd, player_t * player);
+int switch_sdlon_from_set(sdlon sd_in, sdlon sd_out, player_t * player);
 int send_sdlon_in_box(sdlon sd);
 int load_box(char * name);
 int get_player_item(player_t * player, int num_item);
 int add_items(int num_item, int qtt, player_t * player);
 int remove_items(int num_item, int qtt, player_t * player);
 extern int sspi(player_t player);
+extern int display_pss(player_t player);
 #endif
