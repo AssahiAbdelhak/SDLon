1e4732bf09a07bafcd3300f23c839bf0c3b7b0c3
LilianGit <liliancolla@gmail.com>
2023-03-14 10:09:05 +0100

    cinematique

--- sdlon/intro/sdl_intro_test.c
+++ sdlon/intro/sdl_intro_test.c
@@ -90,7 +90,7 @@ int main(int argc, char **argv){
           //img et txt crée et reste tant que aucune touche n'est press
           blackscreen(renderer);
 
-          img = print_image(renderer, "../img/asset/trfront.png", 540, 320, 200, 200);
+          img = print_image(renderer, "../img/asset/SDLon_character/trfront.png", 540, 320, 200, 200);
           txt = print_text(renderer, "RosesareFF0000.ttf", "Bonjour dresseur, je suis le professeur SDLon, mon nom est Ange.", MARGE, LIGNE1, 255, 255, 255);
 
           break;    
@@ -106,7 +106,7 @@ int main(int argc, char **argv){
           }
           test_cond = 0;
 
-          img2 = print_image(renderer, "../img/asset/trfront.png", 540, 320, 200, 200);
+          img2 = print_image(renderer, "../img/asset/SDLon_character/trfront.png", 540, 320, 200, 200);
           txt2 = print_text(renderer, "RosesareFF0000.ttf", "Si tu est ici c'est que tu comptes attraper des SDLon je suppose.", MARGE, LIGNE1, 255, 255, 255);
 
           break;
@@ -121,8 +121,8 @@ int main(int argc, char **argv){
           }
           test_cond = 0;
           
-          img3 = print_image(renderer, "../img/asset/trfront.png", 540, 320, 200, 200);
-          img3_2 = print_image(renderer, "../img/asset/667s.png", 450, 355, 200, 200);
+          img3 = print_image(renderer, "../img/asset/SDLon_character/trfront.png", 540, 320, 200, 200);
+          img3_2 = print_image(renderer, "../img/asset/SDLon_creature/667s.png", 450, 355, 200, 200);
           txt3 = print_text(renderer, "RosesareFF0000.ttf", "Tres bien, vois tu, ceci est un sdlon !", MARGE, LIGNE1, 255, 255, 255);
 
           break;
@@ -137,8 +137,8 @@ int main(int argc, char **argv){
           }
           test_cond = 0;
 
-          img4 = print_image(renderer, "../img/asset/trfront.png", 540, 320, 200, 200);
-          img4_2 = print_image(renderer, "../img/asset/667s.png", 450, 355, 200, 200);
+          img4 = print_image(renderer, "../img/asset/SDLon_character/trfront.png", 540, 320, 200, 200);
+          img4_2 = print_image(renderer, "../img/asset/SDLon_creature/667s.png", 450, 355, 200, 200);
           txt4 = print_text(renderer, "RosesareFF0000.ttf", "Tout d'abord j'ai une question.", MARGE, LIGNE1, 255, 255, 255);
           txt4_2 = print_text(renderer, "RosesareFF0000.ttf", "Est-tu un homme ou une femme ?", MARGE, LIGNE2, 255, 255, 255);
 
@@ -160,8 +160,8 @@ int main(int argc, char **argv){
           }
           test_cond = 0;
 
-          img5 = print_image(renderer, "../img/asset/trfront.png", 540, 320, 200, 200);
-          img5_2 = print_image(renderer, "../img/asset/667s.png", 450, 355, 200, 200);
+          img5 = print_image(renderer, "../img/asset/SDLon_character/trfront.png", 540, 320, 200, 200);
+          img5_2 = print_image(renderer, "../img/asset/SDLon_creature/667s.png", 450, 355, 200, 200);
           txt5 = print_text(renderer, "RosesareFF0000.ttf", "Tres bien, n'oublie pas de passer me voir au labo, j'ai quelques choses", MARGE, LIGNE1, 255, 255, 255);
           txt5_2 = print_text(renderer, "RosesareFF0000.ttf", "a te donner, je te souhaite une bonne aventure", MARGE, LIGNE2, 255, 255, 255);
           //voir programme de abdelhak pour créer un choix via l'écran et sauvegarder la valeur dans le jeu ou un fichier annexe.
