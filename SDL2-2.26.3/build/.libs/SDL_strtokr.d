d998154452e2b4e3629d7870fea69f2253500d9f
Wandrille BALLEREAU <71776091+KirZaping@users.noreply.github.com>
2023-03-22 14:19:19 +0100

    Delete SDL2-2.26.3 directory

--- SDL2-2.26.3/build/.libs/SDL_strtokr.d
+++
@@ -1,8 +0,0 @@
-build/SDL_strtokr.lo: \
- /info/etu/l2info/s2101106/Documents/projet/SDL2-2.26.3/src/stdlib/SDL_strtokr.c \
- /info/etu/l2info/s2101106/Documents/projet/SDL2-2.26.3/src/stdlib/../SDL_internal.h \
- /info/etu/l2info/s2101106/Documents/projet/SDL2-2.26.3/src/stdlib/../dynapi/SDL_dynapi.h \
- /info/etu/l2info/s2101106/Documents/projet/SDL2-2.26.3/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h include/SDL_stdinc.h
