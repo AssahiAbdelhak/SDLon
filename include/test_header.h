af89494750fbc87de75cb1a62e41269bb64021a8
Wandrille BALLEREAU <71776091+KirZaping@users.noreply.github.com>
2023-03-22 14:20:42 +0100

    Delete include directory

--- include/test_header.h
+++
@@ -1,4 +0,0 @@
-void posi_test_print(int test, int verbose);
-void nega_test_print(int test, int verbose);
-void affiche_test(char * str, int verbose);
-void affiche_fiability(char * str, int nb_data, float nb_gen, int verbose);
